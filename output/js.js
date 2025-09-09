08730177f405f1ab969be372b2f1ee035753fc12
truyendam171997-code <damplbl171997@gmail.com>
2025-09-09 22:14:21 +0700

    upload file

---
+++ output/js.js
@@ -0,0 +1,6 @@
+let message = document.getElementById('message');
+message.innerHTML = "xin chao <br>"
+let btnMess = document.getElementById('btnMess');
+btnMess.addEventListener('click', () => {alert("xin chào")})
+console.log("xin chao");
+document.write("xin chao");
