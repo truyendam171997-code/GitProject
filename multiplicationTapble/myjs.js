99d60404d30d3a478c4d4616e89590cddf66743b
truyendam171997-code <damplbl171997@gmail.com>
2025-09-11 19:37:02 +0700

    update

--- multiplicationTapble/myjs.js
+++ multiplicationTapble/myjs.js
@@ -1,6 +1,12 @@
-for (let i = 2; i < 10; i++){
-    for (let j = 1; j < 10; j++){
-        let multi = i * j;
-        document.write( i +" * " + j + "= " + multi+"<br>") ;
+// 2 * 1 = 2   3*1= 3  4*1= 4
+// 2 * 2 = 4   3*2= 6  4*2= 8
+// 2 * 3 = 6   3*3= 9  4*3= 12
+document.write("<table border='1'>");
+for (let i = 1;i <= 10; i++){
+    document.write("<tr>")
+    for (let j = 2; j < 10;j++){
+        document.write("<td>"+j + " * "+ i + " = "+(i*j)+"</td>");
     }
+    document.write("</tr>")
 }
+document.write("</table>");
