be709c0c83a5792a82ef89603173bcd752cd09a3
truyendam171997-code <damplbl171997@gmail.com>
2025-09-14 13:13:23 +0700

    up load

---
+++ varible_operator/varible/varible.js
@@ -0,0 +1,26 @@
+// bài 1
+let i = 10;
+
+let f = 20.5;
+
+let b=  true;
+
+let s = "Hà Nội";
+
+document.write("biến"+ " i " + "= " + i+"<br>")
+
+document.write("biến"+ " f " + "= " + f+"<br>")
+
+document.write("biến"+ " b " + "= " + b+"<br>")
+
+document.write("biến"+ " s " + "= " + s+"<br>")
+
+// bài 2
+let width = 40;
+let height = 20 ;
+let area = width * height;
+document.write("dien tích là "+ area+"<br>")
+//  bài 3
+let x = +prompt("nhap số a: ",)
+let y = +prompt("nhap số b: ",)
+ let z = (x % y == 0) ? document.write(" a là bội sô b "): document.write(" a không phải bội sô b ")
